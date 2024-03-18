2475a65bc2031bc34b80f51118d3f1986f680a9e
IsraelHuentecura <israel.huentecura@mayor.cl>
2024-03-18 11:08:02 -0300

    mejora en gráficas

--- main.py
+++ main.py
@@ -9,11 +9,28 @@ df = pd.read_csv('./data/fcast_delitos_comunas.csv')
 df['ds'] = pd.to_datetime(df['ds'])
 
 
+st.set_page_config(layout = 'wide')
+
 
 
 # Filtro de región
 region = st.sidebar.selectbox('Selecciona una región', ['Región Metropolitana de Santiago',
                                                         'Región de Valparaíso',
+                                                        'Región de Coquimbo',
+                                                        'Región de O’Higgins',
+                                                        'Región del Maule',
+                                                        'Región de Ñuble',
+                                                        'Región del Biobío',
+                                                        'Región de La Araucanía',
+                                                        'Región de Los Ríos',
+                                                        'Región de Los Lagos',
+                                                        'Región de Aysén del General Carlos Ibáñez del Campo',
+                                                        'Región de Magallanes y de la Antártica Chilena',
+                                                        'Región de Arica y Parinacota',
+                                                        'Región de Tarapacá',
+                                                        'Región de Antofagasta',
+                                                        'Región de Atacama'
+                                                        
                                                         ]
                               )
 # Filtro de fecha
@@ -23,6 +40,21 @@ fecha = st.sidebar.selectbox('Selecciona una fecha', fecha_unica)
 dict_regiones = {
     'Región Metropolitana de Santiago': 13,
     'Región de Valparaíso': 5,
+    'Región de Coquimbo': 4,
+    'Región de O’Higgins': 6,
+    'Región del Maule': 7,
+    'Región de Ñuble': 16,
+    'Región del Biobío': 8,
+    'Región de La Araucanía': 9,
+    'Región de Los Ríos': 14,
+    'Región de Los Lagos': 10,
+    'Región de Aysén del General Carlos Ibáñez del Campo': 11,
+    'Región de Magallanes y de la Antártica Chilena': 12,
+    'Región de Arica y Parinacota': 15,
+    'Región de Tarapacá': 1,
+    'Región de Antofagasta': 2,
+    'Región de Atacama': 3,
+    
 }
 
 # Tu objeto GeoJSON desde el path
@@ -43,6 +75,7 @@ gdf['centroid'] = gdf['geometry'].centroid
 gdf['lon'] = gdf['centroid'].x
 gdf['lat'] = gdf['centroid'].y
 
+
 # Crea el gráfico con Plotly y como mapa de calor la variable tasa_dmcs
 map_fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, 
                            locations=gdf.index, 
@@ -52,4 +85,7 @@ map_fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry,
                            center={"lat": gdf['lat'].mean(), "lon": gdf['lon'].mean()},
                            zoom=7,
                            hover_data=['Comuna'])
+
+# Hacer que el grafico más ancho
+map_fig.update_layout(width=1000, height=600)
 st.plotly_chart(map_fig)
--- pages/TimeSeries.py
+++ pages/TimeSeries.py
@@ -2,12 +2,33 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+st.set_page_config(layout = 'wide')
+
+
 df = pd.read_csv('./data/fcast_delitos_comunas.csv')
 
 df['ds'] = pd.to_datetime(df['ds'])
 # Filtro por comuna
 comuna = st.sidebar.selectbox('Selecciona una comuna', df['comuna'].unique())
 df_filter = df[df['comuna'] == comuna]
+
+# Hacer un smooth de la serie de tiempo
+df_filter['yhat'] = df_filter['yhat'].rolling(window=12).mean()
+df_filter['tasa_dmcs'] = df_filter['tasa_dmcs'].rolling(window=12).mean()
+
 # Hacer un gráfico de series de tiempo yhat y tasa_dmcs vs el tiempo
-fig = px.line(df_filter, x='ds', y=['yhat', 'tasa_dmcs'], title='Delitos en la comuna de Santiago')
+fig = px.line(df_filter, x='ds', y=['yhat', 'tasa_dmcs'], title=f'Predicción de delitos de mayor connotación social en {comuna}')
+
+#Hacer el grafico más ancho
+fig.update_layout(width=1000, height=600)
+
+# Poner de nombre fecha en el eje X
+fig.update_xaxes(title_text='Fecha')
+# Poner de nombre Delitos de mayor connotación social en el eje Y
+fig.update_yaxes(title_text='Delitos de mayor connotación social (miles)')
+# En la leyenda de yhat poner 'Predicción'
+fig.for_each_trace(lambda t: t.update(name=t.name.replace("yhat", "Predicción")))
+# En la leyenda de tasa_dmcs poner 'Tasa de delitos'
+fig.for_each_trace(lambda t: t.update(name=t.name.replace("tasa_dmcs", "Tasa de delitos")))
+# Mostrar el gráfico
 st.plotly_chart(fig)
